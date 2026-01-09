faf3aaa04ce302843ac57be8c36d5145132fe4c1
Harsh Singh <harshsinghkumwar97819@gmail.com>
2026-01-09 12:13:35 +0530

    Select + Radio Button

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,18 @@
 import './App.css';
+import { MuiRadioButton } from './components/MuiRadioButton';
+// import { MuiSelect } from './components/MuiSelect';
 // import { MuiButton } from './components/MuiButton';
 // import { MuiTypography } from './components/MuiTypography';
-import { MuiTextField } from './components/MuiTextField';
+// import { MuiTextField } from './components/MuiTextField';
 
 function App() {
   return (
     <div className="App">
       {/* <MuiTypography/> */}
       {/* <MuiButton/> */}
-      <MuiTextField/>
+      {/* <MuiTextField/> */}
+      {/* <MuiSelect/> */}
+      <MuiRadioButton/>
     </div>
   );
 }
