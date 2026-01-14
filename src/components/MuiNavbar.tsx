a756aa92cc5f975181325464a55d6cc4a04a192a
Harsh Singh <harshsinghkumwar97819@gmail.com>
2026-01-14 16:22:42 +0530

    navbar

---
+++ src/components/MuiNavbar.tsx
@@ -0,0 +1,21 @@
+import { AppBar , IconButton, Toolbar , Typography , Stack , Button } from "@mui/material"
+import CatchingPokemonIcon from "@mui/icons-material/CatchingPokemon"
+
+export const MuiNavbar = () => {
+  return (
+    <AppBar position="static">
+        <Toolbar>
+            <IconButton size="large" edge="start" color="inherit" aria-label="logo">
+                <CatchingPokemonIcon/>
+            </IconButton>
+            <Typography variant="h6" component='div' sx={{flexGrow:1}}>POKEMONAPP</Typography>
+            <Stack spacing={2} direction='row'>
+                <Button color="inherit">Features</Button>
+                <Button color="inherit">Pricing</Button>
+                <Button color="inherit">About</Button>
+                <Button color="inherit">Login</Button>
+            </Stack>
+        </Toolbar>
+    </AppBar>
+  )
+}
