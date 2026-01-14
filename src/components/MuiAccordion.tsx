6ee6389274ff2287e7c5dbe397b83cd6780c9673
Harsh Singh <harshsinghkumwar97819@gmail.com>
2026-01-14 12:44:42 +0530

    Image list

--- src/components/MuiAccordion.tsx
+++ src/components/MuiAccordion.tsx
@@ -8,13 +8,16 @@ import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 import { useState } from "react";
 
 export const MuiAccordion = () => {
-    const [expanded , setExpanded] = useState<string | false>(false)
-    const handleChange = (isExpanded:boolean , panel:string) => {
-        setExpanded(isExpanded?panel:false)
-    }   
+  const [expanded, setExpanded] = useState<string | false>(false);
+  const handleChange = (isExpanded: boolean, panel: string) => {
+    setExpanded(isExpanded ? panel : false);
+  };
   return (
     <div>
-      <Accordion expanded={expanded==='panel1'} onChange={(event , isExpanded)=> handleChange(isExpanded , 'panel1')}>
+      <Accordion
+        expanded={expanded === "panel1"}
+        onChange={(event, isExpanded) => handleChange(isExpanded, "panel1")}
+      >
         <AccordionSummary
           id="panel1-header"
           aria-controls="panel1-content"
@@ -31,7 +34,10 @@ export const MuiAccordion = () => {
           </Typography>
         </AccordionDetails>
       </Accordion>
-      <Accordion expanded={expanded==='panel2'} onChange={(event , isExpanded)=> handleChange(isExpanded , 'panel2')}>
+      <Accordion
+        expanded={expanded === "panel2"}
+        onChange={(event, isExpanded) => handleChange(isExpanded, "panel2")}
+      >
         <AccordionSummary
           id="panel2-header"
           aria-controls="panel2-content"
@@ -48,7 +54,10 @@ export const MuiAccordion = () => {
           </Typography>
         </AccordionDetails>
       </Accordion>
-     <Accordion expanded={expanded==='panel3'} onChange={(event , isExpanded)=> handleChange(isExpanded , 'panel3')}>
+      <Accordion
+        expanded={expanded === "panel3"}
+        onChange={(event, isExpanded) => handleChange(isExpanded, "panel3")}
+      >
         <AccordionSummary
           id="panel3-header"
           aria-controls="panel3-content"
