1ccd239bbf9930d15459adce9481a42176e6cabf
Harsh Singh <harshsinghkumwar97819@gmail.com>
2026-01-17 22:56:18 +0530

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,24 @@
 import './App.css';
-import { MuiAlert } from './components/MuiAlert';
-import { MuiAvatar } from './components/MuiAvatar';
-import { MuiBadge } from './components/MuiBadge';
-import { MuiBottomNavigation } from './components/MuiBottomNavigation';
-import { MuiBreadcrumbs } from './components/MuiBreadcrumbs';
-import { MuiChip } from './components/MuiChip';
-import { MuiDialog } from './components/MuiDialog';
-import { MuiDrawer } from './components/MuiDrawer';
-import { MuiList } from './components/MuiList';
-import { MuiLoadingButton } from './components/MuiLoadingButton';
-import { MuiProgress } from './components/MuiProgress';
-import { MuiSkeleton } from './components/MuiSkeleton';
-import { MuiSnackbar } from './components/MuiSnackbar';
-import { MuiSpeedDial } from './components/MuiSpeedDial';
-import { MuiTooltip } from './components/MuiTooltip';
+import { LocalizationProvider } from '@mui/lab';
+import AdapterDateFns from '@mui/lab/AdapterDateFns'
+import { createTheme , colors , ThemeProvider } from '@mui/material';
+import { MuiMasonry } from './components/MuiMasonry';
+import { MuiResponsiveness } from './components/MuiResponsiveness';
+// import { MuiAlert } from './components/MuiAlert';
+// import { MuiAvatar } from './components/MuiAvatar';
+// import { MuiBadge } from './components/MuiBadge';
+// import { MuiBottomNavigation } from './components/MuiBottomNavigation';
+// import { MuiBreadcrumbs } from './components/MuiBreadcrumbs';
+// import { MuiChip } from './components/MuiChip';
+// import { MuiDialog } from './components/MuiDialog';
+// import { MuiDrawer } from './components/MuiDrawer';
+// import { MuiList } from './components/MuiList';
+// import { MuiLoadingButton } from './components/MuiLoadingButton';
+// import { MuiProgress } from './components/MuiProgress';
+// import { MuiSkeleton } from './components/MuiSkeleton';
+// import { MuiSnackbar } from './components/MuiSnackbar';
+// import { MuiSpeedDial } from './components/MuiSpeedDial';
+// import { MuiTooltip } from './components/MuiTooltip';
 // import { MuiLink } from './components/MuiLink';
 // import { MuiNavbar } from './components/MuiNavbar';
 // import { MuiImageList } from './components/MuiImageList';
@@ -30,8 +35,27 @@ import { MuiTooltip } from './components/MuiTooltip';
 // import { MuiTypography } from './components/MuiTypography';
 // import { MuiTextField } from './components/MuiTextField';
 
+const theme = createTheme({
+  status : {
+    danger : '#e53e3e'
+  } ,
+  palette : {
+    secondary : {
+      main : colors.orange[500] 
+    } ,
+    neutral : {
+      main : colors.grey[500] ,
+      darker : colors.grey[700]
+    }
+  }
+}) ;
+
 function App() {
   return (
+    <ThemeProvider theme={theme}>
+
+    <LocalizationProvider dateAdapter={AdapterDateFns}>
+
     <div className="App">
       {/* <MuiTypography/> */}
       {/* <MuiButton/> */}
@@ -62,8 +86,12 @@ function App() {
       {/* <MuiDialog/> */}
       {/* <MuiProgress/> */}
       {/* <MuiSkeleton/> */}
-      <MuiLoadingButton/>
+      {/* <MuiLoadingButton/> */}
+      {/* <MuiMasonry/> */}
+      <MuiResponsiveness/>
     </div>
+    </LocalizationProvider>
+    </ThemeProvider>
   );
 }
 
---
+++ src/components/MuiResponsiveness.tsx
@@ -0,0 +1,34 @@
+import { Box } from "@mui/material";
+import { styled } from "@mui/material";
+
+const StyledBox = styled(Box)(({ theme }) => ({
+  height: "250px",
+  width: "250px",
+//   backgroundColor: theme.status.danger,
+//   backgroundColor: theme.palette.neutral?.main,
+  backgroundColor: theme.palette.neutral?.darker
+}));
+
+export const MuiResponsiveness = () => {
+  return (
+    <>
+      <Box
+        sx={{
+          height: "300px",
+          width: {
+            xs: 100,
+            sm: 200,
+            md: 300,
+            lg: 400,
+            xl: 500,
+          },
+          // bgcolor : 'primary.main' ,
+          // bgcolor : 'secondary.main' ,
+          // bgcolor : 'secondary.light' ,
+          bgcolor: "secondary.dark",
+        }}
+      ></Box>
+      <StyledBox  />
+    </>
+  );
+};
